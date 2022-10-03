16bf87ac5ac9705f09fbb7796d66c9f1c2f023e9
IronJulo <jules.brss@gmail.com>
2022-10-03 16:25:53 +0200

    Better terminal integration

--- terminal.cpp
+++ terminal.cpp
@@ -5,8 +5,13 @@
 Terminal::Terminal(QWidget *parent) : QWidget(parent)
 {
     QStringList qsl = {"-into", QString::number(winId()),
+                       //"-geometry","180x800",
                        "-bg", "black",
-                       "-fg", "green"};
+                       "-fa", "Monospace",
+                       //"-fs", "10",
+                       //"-fullscreen",
+                       "-e", "resize -s 20 315; zsh;"};
+
     process.start("/usr/bin/xterm", qsl);
     process.waitForStarted();
     qDebug() << process.state();
